8584ed20f0226aaf0d7d6e4f3c6ec4520df58030
JUWON-KEVIN-LEE <quf9345@gmail.com>
2018-01-28 20:14:48 +0900

    update

--- app/src/main/java/com/immymemine/kevin/kotlin_mvvm/Utils.kt
+++ app/src/main/java/com/immymemine/kevin/kotlin_mvvm/Utils.kt
@@ -1,9 +1,14 @@
 package com.immymemine.kevin.kotlin_mvvm
 
 import android.databinding.BindingAdapter
+import android.graphics.drawable.Drawable
 import android.util.Log
 import android.widget.ImageView
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
 /**
  * Created by quf93 on 2018-01-26.
@@ -17,5 +22,16 @@ fun loadImage(imageView : ImageView, imageUrl : String) {
 //            .asGif()
             .load(imageUrl)
             .apply(RequestOptions.centerCropTransform())
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(e: GlideException?, model: Any, target: Target<Drawable>, isFirstResource: Boolean): Boolean {
+                    Log.d("JUWONLEE", e!!.message)
+                    return false
+                }
+
+                override fun onResourceReady(resource: Drawable, model: Any, target: Target<Drawable>, dataSource: DataSource, isFirstResource: Boolean): Boolean {
+                    Log.d("JUWONLEE", "Ready")
+                    return false
+                }
+            })
             .into(imageView)
 }
